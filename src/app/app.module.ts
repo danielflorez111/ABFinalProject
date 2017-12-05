22fb87d16ca7d14d8274b961c7042c7ef7838e13
Daniel Felipe Florez Salgado <danielflorez111@hotmail.com>
2017-12-05 17:17:16 -0500

    Trying something

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -30,7 +31,8 @@ import { Error404Component } from './shared/error404/error404.component';
     AppRoutingModule,
     HttpModule,
     RatingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     UsersService
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { IUser } from './user.interface';
 import { Observable } from "rxjs/Observable";
 import 'rxjs/Rx';
@@ -11,20 +12,18 @@ export class UsersService {
   userURL:string;
   previousMatches:IUser[] = [];
 
-  constructor(private http:Http) {
+  constructor(private http:HttpClient) {
     this.usersURL = "https://shared-lunch.firebaseio.com/users.json";
     this.userURL = "https://shared-lunch.firebaseio.com/users";
   }
   
   getUsers(): Observable<any> {
-    return this.http.get(this.usersURL)
-      .map(res => res.json());
+    return this.http.get(this.usersURL);
   }
   
   getUser(id:string): Observable<any> {
     let url = `${ this.userURL }/${ id }.json`;
-    return this.http.get(url)
-      .map(res => res.json());
+    return this.http.get(url);
   }
 
   getPreviousMatches(previousMatches:any): IUser[] {
@@ -37,6 +36,24 @@ export class UsersService {
       });
     }
     return this.previousMatches;
+  }
+
+  test(id:string) {
+    let user;
+    let matches;
+    let matchesInfo = [];
+
+     this.http.get(this.usersURL).map(users => {
+       user = users[id];
+       matches = user.matches;
+
+       for(let i in matches) {
+         //matchesInfo.push(users.id);
+       }
+
+
+        console.log("matches", matches);
+      }).subscribe();
   }
 
 }
